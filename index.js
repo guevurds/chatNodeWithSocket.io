26142f2ec3fb3ea22ac8ddb228473ad7e39c18e6
guevurds <77858314+guevurds@users.noreply.github.com>
2021-01-22 19:21:00 -0300

    versao1.5
    a interface do chat esta melhorada, com limite de caracteres, e um css a mais em algumas partes

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ app.get('/', (req, res) => {
 
 // app.use(express.static('public'))
 
-http.listen(process.env.PORT || 3338, () => {
+http.listen(process.env.PORT || 3000, () => {
     console.log('ta aberto a porcaria do baguio na porta 3000');
     console.log(" "+ new Date);
 })
